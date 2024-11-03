c14b930f34cbe06752858fcae5d65d1f23346bea
Sonya Castro <sonya.castro.gomez@gmail.com>
2024-11-02 23:07:21 -0500

    base layout

---
+++ apps/frontend/src/pages/ClientPage.tsx
@@ -0,0 +1,11 @@
+import BaseLayout from "../layout/BaseLayout";
+
+export const ClientsPage = () => {
+  return (
+    <BaseLayout title="Clients">
+      <p>Welcome to the clients page!</p>
+    </BaseLayout>
+  );
+};
+
+export default ClientsPage;
