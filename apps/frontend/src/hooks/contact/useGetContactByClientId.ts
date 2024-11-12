9f5b74ac532ac857752b591659f7ae3f3b7a14ea
JoseA1m <josealmenarez08@gmail.com>
2024-11-12 14:19:05 -0500

    Fixed

--- apps/frontend/src/hooks/contact/useGetContactByClientId.ts
+++ apps/frontend/src/hooks/contact/useGetContactByClientId.ts
@@ -1,10 +1,10 @@
 import { ApiRequests } from "../../types/api.types";
-import { getContactsByClientId } from "../../services/client.service";
+import { getContactsByClientId } from "../../services/contact.services";
 import { useQuery } from "@tanstack/react-query";
 
 const useGetContactsByClientId = (clientId: string) => {
   return useQuery({
-    queryKey: [ApiRequests.GET_CONTACTS_BY_CLIENT_ID, clientId],
+    queryKey: [ApiRequests.GET_CONTACTS, clientId],
     queryFn: async () => {
       return await getContactsByClientId(clientId);
     }
