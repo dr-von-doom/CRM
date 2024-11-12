a73943b82b3e44aa6bae6f23798a1c81dd5ec220
Darío Mejía <72144279+DarioMejia@users.noreply.github.com>
2024-11-11 20:07:58 -0500

    feature: delete opp

--- apps/frontend/src/services/opportunity.service.ts
+++ apps/frontend/src/services/opportunity.service.ts
@@ -4,6 +4,20 @@ import { OPPORTUNITIES_PAGE_SIZE } from "../utils/const";
 import { requestApi } from "./api";
 
 /**
+ * Fetches an opportunity by ID from the API.
+ *
+ * @param {string} id - Opportunity ID
+ * @returns {Promise<OpportunityType>} - The opportunity data.
+ */
+export const getOpportunityById = async (id: string): Promise<OpportunityType> => {
+  const { body } = await requestApi(ApiRequests.GET_OPPORTUNITY_BY_ID, {
+    pathParams: { id },
+  });
+
+  return body;
+};
+
+/**
  * Fetches opportunities from the API.
  *
  * @param {number} page - The page number to fetch.
@@ -36,4 +50,30 @@ export const getOpportunities = async (
       : null,
     totalCount,
   };
+};
+
+/**
+ * Updates an opportunity in the API.
+ *
+ * @param {string} id - The ID of the opportunity to update.
+ * @param {Partial<OpportunityType>} opportunityData - The new data for the opportunity.
+ * @returns {Promise<OpportunityType>} - The updated opportunity data.
+ */
+export const updateOpportunity = async (
+  id: string,
+  opportunityData: Partial<OpportunityType>
+): Promise<OpportunityType> => {
+  const options = {
+    body: opportunityData,
+    pathParams: { id },
+  };
+
+  const { body } = await requestApi(ApiRequests.UPDATE_OPPORTUNITY, options);
+
+  return body;
+};
+
+export default {
+  getOpportunityById,
+  updateOpportunity,
 };
