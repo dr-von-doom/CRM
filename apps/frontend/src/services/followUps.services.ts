957f7dc7086e9e2be2e38389e38e1fc9776712cb
Sonya Castro <sonya.castro.gomez@gmail.com>
2024-11-17 19:45:33 -0500

    fixes + services + hooks (#47)

--- apps/frontend/src/services/followUps.services.ts
+++ apps/frontend/src/services/followUps.services.ts
@@ -8,12 +8,26 @@ import { ApiRequests } from "../types/api.types";
  * @param {FollowUpType} followUpData - Follow-up data to create.
  * @returns {Promise<FollowUpType>} - Created follow-up data.
  */
-
 export const createFollowUp = async (
   followUpData: FollowUpType
 ): Promise<FollowUpType> => {
   const { body } = await requestApi(ApiRequests.CREATE_FOLLOW_UP, {
     body: followUpData,
+  });
+  return body;
+};
+
+/**
+ * It retrieves all follow-ups from the API.
+ *
+ * @param {string} opportunityId - Opportunity ID to filter follow-ups.
+ * @returns {Promise<FollowUpType[]>} - List of follow-ups.
+ */
+export const getFollowUps = async (
+  opportunityId?: string
+): Promise<FollowUpType[]> => {
+  const { body } = await requestApi(ApiRequests.GET_FOLLOW_UPS, {
+    queryParams: { opportunityId },
   });
   return body;
 };
