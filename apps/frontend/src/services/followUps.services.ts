e58c10958c2a588c80e34cd5c3d4c2d228a27761
Dario Jose Mejia Caballero <72144279+DarioMejia@users.noreply.github.com>
2024-11-18 23:34:49 -0500

    feat: update follow up service

--- apps/frontend/src/services/followUps.services.ts
+++ apps/frontend/src/services/followUps.services.ts
@@ -18,6 +18,27 @@ export const createFollowUp = async (
 };
 
 /**
+ * Update an existing follow-up in the API.
+ *
+ * @param {string} id - ID of the follow-up to update.
+ * @param {Partial<FollowUpType>} followUpData - Partial follow-up data for the update.
+ * @returns {Promise<FollowUpType>} - Updated follow-up data.
+ */
+export const updateFollowUp = async (
+  id: string,
+  followUpData: Partial<FollowUpType>
+): Promise<FollowUpType> => {
+  const options = {
+    body: followUpData,
+    pathParams: { id },
+  };
+
+  const { body } = await requestApi(ApiRequests.UPDATE_FOLLOW_UP, options);
+
+  return body;
+};
+
+/**
  * It retrieves all follow-ups from the API.
  *
  * @param {string} opportunityId - Opportunity ID to filter follow-ups.
