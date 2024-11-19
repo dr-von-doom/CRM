1d3b7d0ead7675ae0720c0dac4e767ea2f9839f5
Dario Jose Mejia Caballero <72144279+DarioMejia@users.noreply.github.com>
2024-11-19 00:11:21 -0500

    Feature/delete follow up (#56)
    * feat: update follow up service
    
    * feat: update follow up hook
    
    * feat: delete follow up function

---
+++ apps/frontend/src/hooks/followUp/useUpdateFollowUp.ts
@@ -0,0 +1,31 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { updateFollowUp } from "../../services/followUps.services";
+import { ApiRequests } from "../../types/api.types";
+import { FollowUpType } from "../../types/followUps.types";
+
+/**
+ * Custom hook for updating a follow-up using React Query's useMutation.
+ */
+const useUpdateFollowUp = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: async ({
+      id,
+      followUpData,
+    }: {
+      id: string;
+      followUpData: Partial<FollowUpType>;
+    }) => {
+      return await updateFollowUp(id, followUpData);
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: [ApiRequests.GET_FOLLOW_UPS] });
+    },
+    onError: (error) => {
+      console.error("Error updating follow-up:", error);
+    },
+  });
+};
+
+export default useUpdateFollowUp;
